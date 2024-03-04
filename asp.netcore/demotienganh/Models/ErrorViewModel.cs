cc2a14ef90c6be07aab7198f8940894e0202c25d
phungduydat <duydatphung7@gmail.com>
2024-03-04 21:59:24 +0700

    wellcomepage

---
+++ asp.netcore/demotienganh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demotienganh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
