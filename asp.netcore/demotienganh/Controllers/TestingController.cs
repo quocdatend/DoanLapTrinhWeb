e2ecf2dcfb64d8eeea57ab15943bc4d91900027e
quocdatend <hngdat@gmail.com>
2024-03-18 16:17:41 +0700

    update code 18_03 page test with me

---
+++ asp.netcore/demotienganh/Controllers/TestingController.cs
@@ -0,0 +1,36 @@
+﻿using demotienganh.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace demotienganh.Controllers
+{
+    public class TestingController : Controller
+    {
+        private readonly ILogger<TestingController> _logger;
+
+        public TestingController(ILogger<TestingController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+        public IActionResult DoTest()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- asp.netcore/demotienganh/obj/Debug/net8.0/demotienganh.AssemblyInfo.cs
+++ asp.netcore/demotienganh/obj/Debug/net8.0/demotienganh.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demotienganh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c9ad41e77cd9b89a55594970e2922d42cc6346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4821786ba261ab891b50ee0a1e7386eca2ac53e")]
 [assembly: System.Reflection.AssemblyProductAttribute("demotienganh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demotienganh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
